6bf6f78aeaa29108fc1a6a74d1e771baec004489
janbarari <mehdimjt@gmail.com>
2022-06-23 08:43:16 +0300

    network module codebase added

---
+++ network/src/main/java/com/doublesymmetry/musicapp/network/NetworkModule.kt
@@ -0,0 +1,35 @@
+package com.doublesymmetry.musicapp.network
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val BASE_URL = "BASE_URL"
+
+    @Provides
+    @Singleton
+    @Named(BASE_URL)
+    fun provideNetworkBaseUrl(): String {
+        return "https://mocky.io/v2/"
+    }
+
+    @Singleton
+    fun provideRetrofit(
+        @Named(BASE_URL) baseUrl: String
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+    }
+
+}
